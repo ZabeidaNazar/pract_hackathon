05d370a3ca705d5e5168367d10cff081f5d45551
andriyqz <andrijlisun92@gmail.com>
2025-10-09 17:48:42 +0300

    feat: added beatufil tables

--- main.cpp
+++ main.cpp
@@ -1,99 +1,220 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
+
 void add_student(vector<vector<string>> &students_table)
 {
-    string student_name;
-    vector<string> student_info;
+    string name;
+    vector<string> info;
+    string grade;
+
+    cout << "\n=========================================\n";
+    cout << "            Add New Student\n";
+    cout << "=========================================\n";
 
     cout << "Enter student name: ";
-    cin >> student_name;
-    
-    student_info.push_back(student_name);
+    getline(cin, name);
+    info.push_back(name);
+
+    cout << "\n--- Enter 3 grades for " << name << " ---\n";
 
     for (int i = 0; i < 3; i++)
     {
-        string student_grade;
-        cout << "Enter student grade: ";
-        cin >> student_grade;
-        student_info.push_back(student_grade);
+        while (true)
+        {
+            cout << "Grade " << (i + 1) << ": ";
+            getline(cin, grade);
+
+            bool valid = true;
+            for (int j = 0; j < (int)grade.size(); j++)
+            {
+                if (!isdigit(grade[j]))
+                {
+                    valid = false;
+                    break;
+                }
+            }
+
+            int num = 0;
+            if (valid)
+                num = stoi(grade);
+
+            if (!valid || num < 0 || num > 100)
+            {
+                cout << "Please enter a valid grade (0-100).\n";
+            }
+            else
+            {
+                info.push_back(grade);
+                break;
+            }
+        }
     }
 
-    students_table.push_back(student_info);
+    students_table.push_back(info);
 
+    cout << "\nStudent '" << name << "' added successfully!\n";
+    cout << "=========================================\n";
 }
 
 void print_table_of_students(const vector<vector<string>> &students_table)
 {
-    cout << "\n----------------------------\n";
-    cout << "Name         | Average Grade\n";
-    cout << "----------------------------\n";
-
-    for (auto student : students_table)
+    if (students_table.empty())
     {
-        string name = student[0];
-        double sum = 0;
-        for (int i = 1; i < student.size(); i++)
-        {
-            sum += stoi(student[i]); // перетворюємо оцінку з string у int
-        }
-        double average = sum / (student.size() - 1);
-
-        cout << name << string(13 - name.size(), ' ') << "| " << average << endl;
+        cout << "\nNo students added yet!\n";
+        return;
     }
 
-    cout << "----------------------------\n";
-}
+    cout << "\n===============================================================================================================\n";
+    cout << "Name" << string(16, ' ') << "| Average Grade      | Grade 1            | Grade 2            | Grade 3            \n";
+    cout << "\n---------------------------------------------------------------------------------------------------------------\n";
 
-void print_students_by_category()
-{
-    cout << "Categories: \nNumber of bad students: \nNumber of medium students: \nNumber of good students: \n";
-}
-
-void get_medium_of_group()
-{
-    cout << "Medium: sum / cout of students";
-}
-
-int print_particular_student_grades(string student_name, vector<vector<string>> &students_table)
-{
     for (int i = 0; i < students_table.size(); i++)
     {
-        if (students_table[i][0] == student_name)
-        {
-            float student_medium_grade = (float) (stoi(students_table[i][1]) + stoi(students_table[i][2]) + stoi(students_table[i][3])) / 3;
+        string name = students_table[i][0];
+        string g1 = students_table[i][1];
+        string g2 = students_table[i][2];
+        string g3 = students_table[i][3];
 
-            cout << student_name << "'s grades: " << students_table[i][1] << " " << students_table[i][2] << " " << students_table[i][3] << endl;
-            cout << student_name << "'s medium grade: " << student_medium_grade;
-            
-            return 0;
+        float avg = (stoi(students_table[i][1]) + stoi(students_table[i][2]) + stoi(students_table[i][3])) / 3.0f;
+        string avg_str = to_string(avg);
+
+        cout << name << string(20 - name.size(), ' ') 
+             << "| " << avg_str << string(19 - avg_str.size(), ' ') 
+             << "| " << g1 << string(19 - g1.size(), ' ') 
+             << "| " << g2 << string(19 - g2.size(), ' ') 
+             << "| " << g3 << string(19 - g3.size(), ' ') 
+             << endl;
+    }
+
+    cout << "===============================================================================================================\n";
+}
+
+void print_students_by_category(const vector<vector<string>> &students_table)
+{
+    if (students_table.empty())
+    {
+        cout << "\nNo students added yet!\n";
+        return;
+    }
+
+    cout << "\n=============================================\n";
+    cout << "Name" << string(16, ' ') << "| Category\n";
+    cout << "---------------------------------------------\n";
+
+    for (int i = 0; i < students_table.size(); i++)
+    {
+        string name = students_table[i][0];
+        string category;
+        
+        float avg = (stoi(students_table[i][1]) \
+                    + stoi(students_table[i][2]) \
+                    + stoi(students_table[i][3])) 
+                    / 3.0f;
+
+        if (avg < 50) category = "Bad student";
+        else if (avg < 80) category = "Medium student";
+        else category = "Excellent student";
+
+        cout << name << string(20 - name.size(), ' ') << "| " << category << endl;
+    }
+
+    cout << "=============================================\n";
+}
+
+void print_particular_student_grades(string name, const vector<vector<string>> &students_table)
+{
+    for (int i = 0; i < (int)students_table.size(); i++)
+    {
+        if (students_table[i][0] == name)
+        {
+            float avg = (stoi(students_table[i][1]) + stoi(students_table[i][2]) + stoi(students_table[i][3])) / 3.0f;
+
+            cout << "\n=============================================\n";
+            cout << "             Student Information\n";
+            cout << "=============================================\n";
+            cout << "Name: " << name << "\n";
+            cout << "---------------------------------------------\n";
+            cout << "| Grade 1 | Grade 2 | Grade 3 |  Average    \n";
+            cout << "---------------------------------------------\n";
+
+            string g1 = students_table[i][1];
+            string g2 = students_table[i][2];
+            string g3 = students_table[i][3];
+
+            cout << "|  " << g1 << string(7 - g1.size(), ' ')
+                 << "|  " << g2 << string(7 - g2.size(), ' ')
+                 << "|  " << g3 << string(7 - g3.size(), ' ')
+                 << "|   " << avg
+                 << endl;
+
+            cout << "---------------------------------------------\n";
+            cout << "Performance: ";
+
+            if (avg < 50)
+                cout << "Bad student";
+            else if (avg < 80)
+                cout << "Medium student";
+            else
+                cout << "Excellent student";
+
+            cout << "\n=============================================\n";
+            return;
         }
     }
 
-    cout << "Student not found!" << endl;
-    return 1;
+    cout << "\n   Student not found!\n";
 }
+
 
 int main()
 {
     vector<vector<string>> students_table;
-    int choice;
+    string choice;
 
     while (true)
     {
-        cout << "\n1 - Add student\n2 - Show table\n0 - Exit\nChoose: ";
-        cin >> choice;
+        cout << "\n=========== MENU ===========\n";
+        cout << "1 - Add student\n";
+        cout << "2 - Show table of averages\n";
+        cout << "3 - Show students by category\n";
+        cout << "4 - Show grades of a student\n";
+        cout << "0 - Exit\n";
+        cout << "============================\n";
+        cout << "Choose: ";
+        getline(cin, choice);
 
-        if (choice == 1)
+        if (choice == "1")
+        {
             add_student(students_table);
-        else if (choice == 2)
+        }
+        else if (choice == "2")
+        {
             print_table_of_students(students_table);
-        else if (choice == 0)
+        }
+        else if (choice == "3")
+        {
+            print_students_by_category(students_table);
+        }
+        else if (choice == "4")
+        {
+            string name;
+            cout << "Enter student name: ";
+            getline(cin, name);
+            print_particular_student_grades(name, students_table);
+        }
+        else if (choice == "0")
+        {
+            cout << "Exiting...\n";
             break;
+        }
         else
+        {
             cout << "Invalid choice!\n";
+        }
     }
 
     return 0;
