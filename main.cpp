a4bff372fa43f255280dcf7953d9e24953635a81
andriyqz <andrijlisun92@gmail.com>
2025-10-09 10:51:27 +0300

    Added basic template for other teammates

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <vector>
 
+using namespace std;
+
+void add_student()
+{
+    string student_name;
+
+    cout << "Enter student name: ";
+    cin >> student_name;
+
+    cout << student_name;
+}
+
+void print_table_of_students()
+{
+    cout << "Table";
+}
+
+void print_students_by_category()
+{
+    cout << "Categories: \nNumber of bad students: \nNumber of medium students: \nNumber of good students: \n";
+}
+
+void get_medium_of_group()
+{
+    cout << "Medium: sum / cout of students";
+}
+
+void print_particular_student_grades()
+{
+    cout << "Andrew's grade: ";
+}
 
 int main()
-{ 
+{
+    std::vector<std::string> students_table;
+
+
     return 0;
 }
